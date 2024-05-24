3a1ba748ccbb25c8bd8b33e315e1d2c0439de121
SANJAY N <sanjaysumithra2004@gmail.com>
2024-05-24 16:42:54 +0530

    Check error

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,9 @@
 // import Image from "next/image";
 
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       <h1>sanjay</h1>
-//     </main>
-//   );
-// }
-// pages/_app.tsx
-
-import { AppProps } from "next/app";
-import "../styles/globals.css";
-import Header from "./Components/Header/header";
-
-function MyApp({ Component, pageProps }: AppProps) {
+export default function Home() {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>sanjay</h1>
+    </main>
   );
 }
-
-export default MyApp;
