f0731753ef1d635c1e2bc343c6cc3b8686850e83
SANJAY N <sanjaysumithra2004@gmail.com>
2024-05-24 16:36:21 +0530

    feat

---
+++ app/Components/Header/header.tsx
@@ -0,0 +1,33 @@
+// components/Header.tsx
+
+import React from "react";
+import Link from "next/link";
+import "./header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 // import Image from "next/image";
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//       <h1>sanjay</h1>
+//     </main>
+//   );
+// }
+// pages/_app.tsx
+
+import { AppProps } from "next/app";
+import "../styles/globals.css";
+import Header from "./Components/Header/header";
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>sanjay</h1>
-    </main>
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
   );
 }
+
+export default MyApp;
