4fced16944c68cee9ac4658160e6daecf7f1e6bf
SANJAY N <sanjaysumithra2004@gmail.com>
2024-05-24 14:58:14 +0530

    added heading

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Welcome to sanjay's world!!</p>
+      <h1>sanjay</h1>
     </main>
   );
 }
